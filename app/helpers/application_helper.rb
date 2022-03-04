eb8c589dfa0b5f7f793f4f3f79bbabe4d130faed
Lance Liu <lanceliumeng89@gmail.com>
2022-03-04 19:39:16 +1100

    delete header file, and put all code into nav file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    # this is for flash msg helper method, you can find it in views/shared/_messages.html.erb
+    def select_alert_subclass(input)
+        case input
+        when 'notice'
+            return 'success'
+        when 'alert'
+            return 'warning'
+        when 'error'
+            return 'danger'
+        else
+            return 'info'
+        end
+    end
+
 end
