524041ddeb412c75d6c3dd108e6c7fe23c0a3b13
Lance Liu <lanceliumeng89@gmail.com>
2022-03-05 18:39:14 +1100

    update typed.js content to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,7 @@ class StaticPagesController < ApplicationController
     @products = Product.all.limit(3)
     # for landing_page (home page) new trips :desc will loadying the new adds trips from DB
     @new_products= Product.all.limit(3).order(created_at: :desc)
+
   end
 
   def privacy_policy
