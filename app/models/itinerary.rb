41d98cf849a6dfe7794dc1a0ead6bd9347b7cb60
Lance Liu <lanceliumeng89@gmail.com>
2022-03-09 13:25:34 +1100

    add rich text area for itinerary content and breadcrumbs for itinerary show page

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,6 +1,7 @@
 class Itinerary < ApplicationRecord
   belongs_to :trip # trips and itineraries table relationship
   validates :title, :content, :trip, presence: true  #these columns cannot blank
+  has_rich_text :content  #=> for rails action text, when we create new itinerary, content area has more powful function to edit text 
 
    # for firendly_id gem usage, after use this(another migration), the trip url doesn't show id, good for security
    extend FriendlyId
