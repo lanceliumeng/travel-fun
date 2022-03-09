402b64ed9469f84c63d2a58571a508ee7931275e
Lance Liu <lanceliumeng89@gmail.com>
2022-03-09 12:51:55 +1100

    add public_activity gem set up for itinerary model, try to create itineraries into trips and it is work ok

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -5,5 +5,13 @@ class Itinerary < ApplicationRecord
    # for firendly_id gem usage, after use this(another migration), the trip url doesn't show id, good for security
    extend FriendlyId
    friendly_id :title, use: :slugged
-   
+
+   # public_activity gem setup in trip model
+   include PublicActivity::Model
+   tracked owner: Proc.new{ |controller, model| controller.current_user }
+
+   def to_s
+    title
+   end
+
 end
