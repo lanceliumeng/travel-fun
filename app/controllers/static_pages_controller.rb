27615e798da8d785be1b9c745d7ea19c1a72b407
Lance Liu <lanceliumeng89@gmail.com>
2022-03-13 18:59:49 +1100

    update Carousel for home page, create new partial page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,12 @@ class StaticPagesController < ApplicationController
     @trips = Trip.all.limit(3)
     # for landing_page (home page) new trips :desc will loadying the new adds trips from DB
     @new_trips= Trip.all.limit(3).order(created_at: :desc)
-
+    # for landing_page (home page) show brought trips
+    @purchased_trips = Trip.joins(:orders).where(orders: {user: current_user}).order(created_at: :desc).limit(3)
+    # for landing_page (home page) show popular trips
+    @popluar_trips = Trip.order(orders_count: :desc, created_at: :desc).limit(3)
+    # for landing_page (home page) show top rated trips
+    @top_rated_trips = Trip.order(average_rating: :desc, created_at: :desc).limit(3)
   end
 
   # public_activity gem setup, puls track_activities views and routes, we can display uses CRUD history 
