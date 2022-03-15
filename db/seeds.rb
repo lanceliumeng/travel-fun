3c82a94a24fa37849428c0bb4dd872fb82f86217
Lance Liu <lanceliumeng89@gmail.com>
2022-03-15 20:36:28 +1100

    deploy is ok but no admin account, try to commit all seed file and see what happen

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # create devise user for testing
-User.create(email:'admin@test.com', password:'coolguy', password_confirmation:'coolguy')
+# User.create(email:'admin@test.com', password:'coolguy', password_confirmation:'coolguy')
 
-PublicActivity.enabled = false
-# random db for first test:
-2.times do 
-    Trip.create([{
-        title: Faker::FunnyName.three_word_name,
-        description: Faker::TvShows::AquaTeenHungerForce.quote,
-        # user_id: User.first.id,
-        user: User.find_by(email: "admin@test.com"),
-        brief_info: Faker::Quote.famous_last_words,
-        duration: '5-Days-4-Nights',
-        language: 'English',
-        price: Faker::Number.between(from: 1000, to: 20000)
-    }])
-end
-PublicActivity.enabled = true
+# PublicActivity.enabled = false
+# # random db for first test:
+# 2.times do 
+#     Trip.create([{
+#         title: Faker::FunnyName.three_word_name,
+#         description: Faker::TvShows::AquaTeenHungerForce.quote,
+#         # user_id: User.first.id,
+#         user: User.find_by(email: "admin@test.com"),
+#         brief_info: Faker::Quote.famous_last_words,
+#         duration: '5-Days-4-Nights',
+#         language: 'English',
+#         price: Faker::Number.between(from: 1000, to: 20000)
+#     }])
+# end
+# PublicActivity.enabled = true
