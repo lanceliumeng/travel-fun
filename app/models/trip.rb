f1075d7975917bd6b798a09918ee5488bf806dbf
Lance Liu <lanceliumeng89@gmail.com>
2022-03-17 16:03:49 +1100

    adjust avatar size limit

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -22,7 +22,7 @@ class Trip < ApplicationRecord
     scope :unapproved, -> { where(approved: false) }
 
     has_one_attached :avatar  #=> for attaching files to Records, it sets up one to one mapping between records and files
-    validates :avatar, content_type: ['image/png', 'image/jpeg', 'image/jpg'], size: { less_than: 700.kilobytes , message: 'image size is too large, it should less than 5 MB' } #attached: true,
+    validates :avatar, content_type: ['image/png', 'image/jpeg', 'image/jpg'], size: { less_than: 8000.kilobytes , message: 'image size is too large, it should less than 8 MB' } #attached: true,
     #=> for Active Storage Validations gem setup,only can upload images with 3 types, and cannot oversize
 
     def to_s
